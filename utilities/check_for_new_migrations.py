934e59df7d942e39c29a92090d6b1916894bae0a
ValtteriK <wr1337@protonmail.ch>
2022-01-19 19:18:40 +0200

    Added check for new migrations utility

---
+++ utilities/check_for_new_migrations.py
@@ -0,0 +1,22 @@
+from re import sub
+import subprocess
+import sys
+
+NO_CHANGES_STRING = "No changes detected"
+CREATE_MIGRATIONS_COMMAND = "python ../web/manage.py makemigrations"
+
+def check_for_migrations():
+    procsess = subprocess.Popen(CREATE_MIGRATIONS_COMMAND, shell=True, stdout=subprocess.PIPE)
+    subprocess_return_string = procsess.stdout.read().decode("utf-8")
+    if subprocess_return_string is not NO_CHANGES_STRING:
+        return True
+    return False
+
+
+def main() -> None:
+    if check_for_migrations():
+        sys.exit(1)
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
