922f79a750186ea7622f8693a50b6b5fa2d44364
Martta <martta_1994@hotmail.com>
2022-01-21 18:20:22 +0200

    Add models for basic features

--- web/web/settings.py
+++ web/web/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bobapp.apps.BobappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
