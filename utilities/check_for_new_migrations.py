7f05b46f0f0fc217cd0b157e6e23435a591223f8
Valtteri Kostiainen <wr1337@protonmail.ch>
2022-01-22 18:24:53 +0200

    Migration utilities (#40)
    * Added check for new migrations utility
    
    * Fix typo
    
    * Updated tests to run in current project structure
    
    * Add clean up for tests
    
    * enable all tests
    
    * Add migration check step to qualit gate
    
    * Fix Qualit Gate
    
    * Test model change
    
    * revert test model change
    
    * Added coverage to migration check script
    
    * Fix typo
    
    * assertEquals is deprecated
    
    * Added utilities module to sonar config
    
    * Increase test coverage

---
+++ utilities/check_for_new_migrations.py
@@ -0,0 +1,22 @@
+from re import sub
+import subprocess
+import sys
+
+NO_CHANGES_STRING = "No changes detected"
+CREATE_MIGRATIONS_COMMAND = "python ../web/manage.py makemigrations"
+
+def uncreated_migrations_exist():
+    with subprocess.Popen(CREATE_MIGRATIONS_COMMAND, shell=True, stdout=subprocess.PIPE) as process:
+        process.wait()
+        subprocess_return_string = process.stdout.read().decode("utf-8")
+    if NO_CHANGES_STRING not in subprocess_return_string:
+        return True
+    return False
+
+
+def main() -> None:
+    if uncreated_migrations_exist():
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
