e54ce567bbd9d41866db81617028b278bc2b3cdb
Martta <10349883+M4R774@users.noreply.github.com>
2022-01-22 14:29:09 +0200

    Change production to docker-compose to make database persistent (#37)

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,2 @@
 echo "Starting deployment"
-docker build . -t bobweb
-docker stop bobweb
-docker rm bobweb
-docker run --restart=always -d --name bobweb bobweb
+docker-compose up --build --detach --force-recreate --remove-orphans
