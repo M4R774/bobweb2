9a27d518c5ded601b572e54b10d2ce4e6f04204c
Martta <martta_1994@hotmail.com>
2022-01-29 00:06:37 +0200

    Give promotions to committers weekly

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Starting deployment"
-BROADCAST_MESSAGE=$(git log -1 --pretty=%B)
-export BROADCAST_MESSAGE
+COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+COMMIT_AUTHOR_NAME=$(git log -1 --pretty=%an)
+COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty=%ae)
+export COMMIT_MESSAGE
+export COMMIT_AUTHOR_NAME
+export COMMIT_AUTHOR_EMAIL
 docker-compose up --build --detach --force-recreate --remove-orphans
