1136450569dabae381bda7755709b961e0e30f9e
Martta <martta_1994@hotmail.com>
2022-01-29 23:06:55 +0200

    Ota varmuuskopio aina ennen uuden version käynnistämistä

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "Taking back ups from the db"
+mkdir -p ../backups
+cp web/db.sqlite3 ../backups/$(date +%F_%R).sqlite3
+
 echo "Starting deployment"
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 COMMIT_AUTHOR_NAME=$(git log -1 --pretty=%an)
@@ -6,4 +11,5 @@ COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty=%ae)
 export COMMIT_MESSAGE
 export COMMIT_AUTHOR_NAME
 export COMMIT_AUTHOR_EMAIL
+
 docker-compose up --build --detach --force-recreate --remove-orphans
