4966306b680098d743d0b400701663c89de76737
Martta <martta_1994@hotmail.com>
2022-02-04 22:57:40 +0200

    Heippa maailma nettisivujen testi

--- web/web/settings.py
+++ web/web/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# TODO: When everything works, change this to martta.tk or whatever
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -118,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
