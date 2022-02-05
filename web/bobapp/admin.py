6b997cf3c7a74e71143967fea9554afce0156d8f
Martta <10349883+M4R774@users.noreply.github.com>
2022-02-05 23:24:00 +0200

    Säädä CSRF ja CORS asetuksia ja lisää bobin tietokanta admin paneeliin (#120)

--- web/bobapp/admin.py
+++ web/bobapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TelegramUser, GitUser, Chat, ChatMember, Proverb, ChatProverb, Reminder
+
+admin.site.register(TelegramUser)
+admin.site.register(GitUser)
+admin.site.register(Chat)
+admin.site.register(ChatMember)
+admin.site.register(Proverb)
+admin.site.register(ChatProverb)
+admin.site.register(Reminder)
