67ef0dcc24a485f12b6c35c7f6f40c3305dae4b6
taturl <taturl@github.com>
2022-09-17 20:14:13 +0300

    Add pythonpath

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 bobweb/web/manage.py migrate --no-input
-python3 bobweb/bob/main.py
+export PYTHONPATH=$(pwd)
+python bobweb/web/manage.py migrate --no-input
+python bobweb/bob/main.py
