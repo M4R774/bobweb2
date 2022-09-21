5f131744c40b436f086ee459153e53fbff8dd26d
Latemus <lauri_viitanen@hotmail.com>
2022-09-21 18:42:04 +0300

    #19 Lisätty bobin oikea nimi

--- bobweb/bob/resources/bob_constants.py
+++ bobweb/bob/resources/bob_constants.py
@@ -1,4 +1,4 @@
 COMMAND_PREFIXES = ['.', '/', '!']  # List of supported prefixes
 PREFIXES_MATCHER = '[{}]'.format(''.join(COMMAND_PREFIXES))  # prefixes as regex matcher
 DEFAULT_TIMEZONE = 'Europe/Helsinki'  # Default timezone for bot
-BOT_USERNAME = "LatenTestiBot"
+BOT_USERNAME = "BandOfBrothersBot"
