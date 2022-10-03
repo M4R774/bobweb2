2c3c87ab7b537deb197674074d65dd3920ba69e7
Latemus <lauri_viitanen@hotmail.com>
2022-10-03 08:32:01 +0300

    Poistettu joka aamuinen "HYVÄÄ HUOMENTA!" viestin lähetys.

--- bobweb/bob/scheduler.py
+++ bobweb/bob/scheduler.py
@@ -20,6 +20,16 @@ class Scheduler:
         # Nice website for building cron schedules: https://crontab.guru/#0_16_*_*_5
         # NOTE: Seconds is the LAST element, while minutes is the FIRST
         # eg. minute hour day(month) month day(week) second
+        #
+        # For example:
+        # cron_every_morning = '0 8 * * *'  # “Every day at 08:00.”
+        # self.friday_noon_task = aiocron.crontab(str(cron_every_morning),
+        #                                         func=self.good_morning_broadcast,
+        #                                         start=True,
+        #                                         tz=tz)
+        #
+        # async def good_morning_broadcast(self):
+        #     await main.broadcast(self.updater.bot, "HYVÄÄ HUOMENTA!")
 
         tz = pytz.timezone(DEFAULT_TIMEZONE)
 
@@ -29,21 +39,11 @@ class Scheduler:
                                                 start=True,
                                                 tz=tz)
 
-        cron_every_morning = '0 8 * * *'  # “Every day at 08:00.”
-        self.friday_noon_task = aiocron.crontab(str(cron_every_morning),
-                                                func=self.good_morning_broadcast,
-                                                start=True,
-                                                tz=tz)
-
         asyncio.get_event_loop().run_forever()
 
     async def friday_noon(self):
         # TODO: Perjantain rankkien lähetys
         await db_backup.create(self.updater.bot)
         await main.broadcast(self.updater.bot, "Jahas, työviikko taas pulkassa,,,")
-
-    async def good_morning_broadcast(self):
-        # TODO: Daily digest
-        await main.broadcast(self.updater.bot, "HYVÄÄ HUOMENTA!")
 
     #TODO: Muistutus feature
