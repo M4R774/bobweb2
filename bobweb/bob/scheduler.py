6f456dd9c538a5e74dda93b4f071a9f87f5bcb30
Lauri Viitanen <lauri_viitanen@hotmail.com>
2023-10-11 22:23:38 +0300

    epic games announcement improvements - split `broadcast` method to two. One that broadcasts to all chats with broadcasting set on and one that broadcasts to all given chats - changed epic games scheduled free games announcement check to be run on every day   - scheduled job now checks for new free games startiung from 18:00:30. New games are checked once a minute for 5 tries. For each promotion checks that if it's starting date is current date, it is broadcasted as new free game promotion even though it is not thursday. If no new free games are found on thursday when new promotions are expected, broadcasts no-games-message after 5 tries

--- bobweb/bob/scheduler.py
+++ bobweb/bob/scheduler.py
@@ -1,19 +1,16 @@
 import datetime
 import logging
 
-from aiohttp import ClientResponseError
-from telegram.constants import ParseMode
 from telegram.ext import Application, CallbackContext
 import signal  # Keyboard interrupt listening for Windows
 
-from bobweb.bob.command_epic_games import create_free_games_announcement_msg, fetch_failed_msg
+from bobweb.bob.command_epic_games import daily_announce_new_free_epic_games_store_games
 from bobweb.bob.git_promotions import broadcast_and_promote
 from bobweb.bob.resources.bob_constants import fitz
-from bobweb.bob.utils_common import has
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-from bobweb.bob import database, broadcaster, nordpool_service
+from bobweb.bob import broadcaster, nordpool_service
 from bobweb.bob import db_backup
 
 logger = logging.getLogger(__name__)
@@ -39,7 +36,9 @@ class Scheduler:
     Class for all scheduled task and background services.
 
     Since update 13.0 -> 20.5 all scheduled tasks are handled with PTB library's JobQueue
-    Documentation: https://docs.python-telegram-bot.org/en/v20.5/telegram.ext.jobqueue.html
+    JobQueue documentation: https://docs.python-telegram-bot.org/en/v20.5/telegram.ext.jobqueue.html
+
+    Cron syntax codumentation: https://apscheduler.readthedocs.io/en/stable/modules/triggers/cron.html
 
     Example:
     “Every day at 08:00.”
@@ -54,12 +53,13 @@ class Scheduler:
         # First invoke all jobs that should be run at startup and then add recurrent tasks
         # At the startup do broadcast and promote action immediately once
         application.job_queue.run_once(broadcast_and_promote, 0)
+        application.job_queue.run_once(daily_announce_new_free_epic_games_store_games, 5)
 
-        # 'At 18:05 on Thursday.'
-        application.job_queue.run_daily(days=THURSDAY, time=datetime.time(hour=18, minute=5, tzinfo=fitz),
-                                        callback=announce_free_epic_games_store_games)
+        # Is started Every day at 18:00:30
+        application.job_queue.run_daily(days=EVERY_WEEK_DAY, time=datetime.time(hour=18, minute=0, second=30, tzinfo=fitz),
+                                        callback=daily_announce_new_free_epic_games_store_games)
 
-        # “At 17:00 on Friday.”
+        # At 17:00 on Friday
         application.job_queue.run_daily(days=FRIDAY, time=datetime.time(hour=17, minute=0, tzinfo=fitz),
                                         callback=friday_noon)
 
@@ -72,23 +72,3 @@ class Scheduler:
 async def friday_noon(context: CallbackContext):
     await db_backup.create(context.bot)
     await broadcaster.broadcast(context.bot, "Jahas, työviikko taas pulkassa,,,")
-
-
-async def announce_free_epic_games_store_games(context: CallbackContext):
-    chats_with_announcement_on = [x for x in database.get_chats() if x.free_game_offers_enabled]
-    if len(chats_with_announcement_on) == 0:
-        return  # Early return if no chats with
-
-    # Define either announcement message and possible game images or fetch_failed_msg without image
-    try:
-        msg, image_bytes = await create_free_games_announcement_msg()
-    except ClientResponseError as e:
-        log_msg = f'Epic Games Api error. [status]: {str(e.status)}, [message]: {e.message}, [headers]: {e.headers}'
-        logger.exception(log_msg, exc_info=True)
-        msg, image_bytes = fetch_failed_msg, None
-
-    for chat in chats_with_announcement_on:
-        if has(image_bytes):
-            await context.bot.send_photo(chat_id=chat.id, photo=image_bytes, caption=msg, parse_mode=ParseMode.HTML)
-        else:
-            await context.bot.send_message(chat_id=chat.id, text=msg, parse_mode=ParseMode.HTML)
--- bobweb/bob/test_command_epic_games.py
+++ bobweb/bob/test_command_epic_games.py
@@ -1,3 +1,4 @@
+import asyncio
 import io
 import json
 from typing import List
@@ -6,28 +7,32 @@ from unittest import mock
 
 import django
 import pytest
+from aiohttp import ClientResponseError
 from django.core import management
 from django.test import TestCase
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 import requests
 from PIL.Image import Image
+from freezegun import freeze_time
 from requests import Response
+from telegram.ext import CallbackContext
 
-from bobweb.bob import command_epic_games
+from bobweb.bob import command_epic_games, database
 from bobweb.bob.command_epic_games import epic_free_games_api_endpoint, EpicGamesOffersCommand, \
-    get_product_page_or_deals_page_url
+    get_product_page_or_deals_page_url, daily_announce_new_free_epic_games_store_games
 from bobweb.bob.test_command_kunta import create_mock_image
 from bobweb.bob.tests_mocks_v2 import init_chat_user
-from bobweb.bob.tests_utils import assert_command_triggers, mock_request_raises_client_response_error, mock_fetch_json_with_content
+from bobweb.bob.tests_utils import assert_command_triggers, mock_request_raises_client_response_error, \
+    mock_fetch_json_with_content, AsyncMock
 
 
-async def mock_fetch_json(urls: str, *args, **kwargs):
-    if 'freeGamesPromotions' in urls:
+async def mock_fetch_json(url: str, *args, **kwargs):
+    if 'freeGamesPromotions' in url:
         # first api call that gets the promotion date
         with open('bobweb/bob/resources/test/epicGamesFreeGamesPromotionsExample.json') as example_json:
             return json.loads(example_json.read())
-    elif urls.endswith('.png'):
+    elif url.endswith('.png'):
         # Game offer image request -> Create a mock response with appropriate content
         img: Image = create_mock_image(*args, **kwargs)
         img_byte_array = io.BytesIO()
@@ -39,8 +44,25 @@ async def mock_fetch_all_content_bytes(urls: List[str], *args, **kwargs):
     return [await mock_fetch_json(url) for url in urls]
 
 
+async def mock_fetch_raises_client_response_error(*args, **kwargs):
+    raise ClientResponseError(status=-1, message='-1', headers={'a': 1}, request_info=None, history=None)
+
+
+class MockApi:
+    """ Mock api that only returns requested response at third call """
+    call_count = 0
+
+    async def mock_fetch_succeed_on_third_call(*args, **kwargs):
+        MockApi.call_count += 1
+        if MockApi.call_count >= 3:
+            return await mock_fetch_json(*args, **kwargs)
+        else:
+            return await mock_fetch_raises_client_response_error(*args, **kwargs)
+
+
 class EpicGamesApiEndpointPingTest(TestCase):
     """ Smoke test against the real api """
+
     async def test_epic_games_api_endpoint_ok(self):
         res: Response = requests.get(epic_free_games_api_endpoint)  # Synchronous requests-library call is OK here
         self.assertEqual(200, res.status_code)
@@ -86,3 +108,84 @@ class EpicGamesBehavioralTests(django.test.TransactionTestCase):
         expected = 'https://store.epicgames.com/en-US/free-games'
         actual = get_product_page_or_deals_page_url(None)
         self.assertEqual(expected, actual)
+
+
+@pytest.mark.asyncio
+@mock.patch('asyncio.sleep', new_callable=AsyncMock)
+@mock.patch('bobweb.bob.async_http.fetch_json', mock_fetch_json)
+@mock.patch('bobweb.bob.async_http.fetch_all_content_bytes', mock_fetch_all_content_bytes)
+class EpicGamesDailyAnnounceTests(django.test.TransactionTestCase):
+    chat = None
+    cb = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(EpicGamesDailyAnnounceTests, cls).setUpClass()
+        management.call_command('migrate')
+
+    def setUp(self):
+        chat, user = init_chat_user()
+        asyncio.run(user.send_message('hi'))
+        chat_in_db = database.get_chat(chat.id)
+        chat_in_db.free_game_offers_enabled = True
+        chat_in_db.save()
+
+        cb: CallbackContext = Mock(spec=CallbackContext)
+        cb.bot = chat.bot
+
+        self.chat = chat
+        self.cb = cb
+
+    @freeze_time('2023-01-01')
+    async def test_no_new_offers_not_thursday(self, _):
+        with self.assertLogs(level='INFO') as log:
+            await daily_announce_new_free_epic_games_store_games(self.cb)
+            self.assertIn('status fetched successfully but no new free games found', log.output[-1])
+
+            # No messages as it's not thursday
+            self.assertEqual([], self.chat.bot.messages)
+
+    @freeze_time('2023-01-05')
+    async def test_no_new_offers_its_thursday(self, _):
+        with self.assertLogs(level='INFO') as log:
+            await daily_announce_new_free_epic_games_store_games(self.cb)
+            self.assertIn('status fetched successfully but no new free games found', log.output[-1])
+
+            # Should have message, as it is expected to have new offers on thursday
+            self.assertEqual('Uusia ilmaisia eeppisiä pelejä ei ole tällä hetkellä tarjolla 👾', self.chat.last_bot_txt())
+
+    @freeze_time('2023-01-01')
+    async def test_api_get_request_is_called_repeatedly_if_it_fails(self, sleep_mock):
+        asyncio_fetch_mock = AsyncMock()
+        asyncio_fetch_mock.return_value = await mock_fetch_json('freeGamesPromotions')
+        with mock.patch('bobweb.bob.async_http.fetch_json', asyncio_fetch_mock) as fetch_mock:
+            await daily_announce_new_free_epic_games_store_games(self.cb)
+            # Fetch is expected to be called 5 times as
+            self.assertEqual(5, fetch_mock.call_count)
+            # And sleep should have been called 4 times with parameter of 60 seconds
+            self.assertEqual(4, sleep_mock.call_count)
+            self.assertEqual(60, sleep_mock.mock_calls[-1].args[-1])
+
+    async def test_client_response_error(self, _):
+        with (
+            self.assertLogs(level='ERROR') as log,
+            mock.patch('bobweb.bob.async_http.fetch_json', mock_fetch_raises_client_response_error)
+        ):
+            await daily_announce_new_free_epic_games_store_games(self.cb)
+            # Should log an error to log and give user-friendly notification that fetch has failed
+            self.assertIn('Epic Games Api error. [status]: -1, [message]: -1, [headers]: {\'a\': 1}', log.output[-1])
+            self.assertEqual('Ilmaisten eeppisten pelien haku epäonnistui 🔌✂️', self.chat.last_bot_txt())
+
+
+    @freeze_time('2023-01-19')  # Date on which there is a starting free game promotion in the test data
+    async def test_fetch_succeeds_on_third_try(self, _):
+        mock_api = MockApi
+        with (
+            mock.patch('bobweb.bob.async_http.fetch_json', mock_api.mock_fetch_succeed_on_third_call),
+            self.assertNoLogs(logger=command_epic_games.logger)  # And there are no messages logged
+        ):
+            await daily_announce_new_free_epic_games_store_games(self.cb)
+            # Check that expected game name is in response
+            self.assertIn('Epistory - Typing Chronicles', self.chat.last_bot_txt())
+            # Mock api has been called only three times, as the third time succeeds
+            self.assertEqual(3, mock_api.call_count)
