fb9888f79785d09d6117c99a3cde109aa3cc3da5
Latemus <lauri_viitanen@hotmail.com>
2023-10-12 17:28:37 +0300

    epic games announcement improvements (#251)
    * epic games announcement improvements
    - split `broadcast` method to two. One that broadcasts to all chats with broadcasting set on and one that broadcasts to all given chats
    - changed epic games scheduled free games announcement check to be run on every day
      - scheduled job now checks for new free games startiung from 18:00:30. New games are checked once a minute for 5 tries. For each promotion checks that if it's starting date is current date, it is broadcasted as new free game promotion even though it is not thursday. If no new free games are found on thursday when new promotions are expected, broadcasts no-games-message after 5 tries
    
    * epic games announcement improvements
    - added catch all error handler

--- bobweb/bob/broadcaster.py
+++ bobweb/bob/broadcaster.py
@@ -1,28 +1,43 @@
 import logging
+from typing import List
 
 import telegram
 from telegram import Bot
+from telegram.constants import ParseMode
 
 from bobweb.bob import database
-
+from bobweb.web.bobapp.models import Chat
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)  #NOSONAR
 logger = logging.getLogger(__name__)
 
 
 async def broadcast(bot: Bot, text: str):
+    """ Broadcasts fo all chats known to bot that have broadcast enabled """
+    chats = [chat for chat in database.get_chats() if chat.broadcast_enabled]
+    await broadcast_to_chats(bot, chats, text)
+
+
+async def broadcast_to_chats(bot: Bot,
+                             chats: List[Chat],
+                             text: str,
+                             image_bytes: bytes = None,
+                             parse_mode: ParseMode = None):
+    """ Broadcasts given message to all given chats. Sends image if given as parameter """
     if text is not None and text != "":
-        chats = database.get_chats()
         for chat in chats:
-            if chat.broadcast_enabled:
-                try:
-                    await bot.send_message(chat_id=chat.id, text=text)
-                except telegram.error.BadRequest as e:
-                    logger.error("Tried to broadcast to chat with id " + str(chat.id) +
-                                 " but Telegram-API responded with \"BadRequest: " + str(e) + "\"")
-                except telegram.error.Forbidden as e2:
-                    logger.error("Tried to broadcast to chat with id " + str(chat.id) +
-                                 " but Telegram-API responded with \"BadRequest: " + str(e2) + "\""
-                                 "User has propably blocked bot so broadcast is disabled in the chat.")
-                    chat.broadcast_enabled = False
-                    chat.save()
+            try:
+                if image_bytes is not None:
+                    await bot.send_photo(chat_id=chat.id, photo=image_bytes, caption=text, parse_mode=parse_mode)
+                else:
+                    await bot.send_message(chat_id=chat.id, text=text, parse_mode=parse_mode)
+            except telegram.error.BadRequest as e:
+                logger.error("Tried to broadcast to chat with id " + str(chat.id) +
+                             " but Telegram-API responded with \"BadRequest: " + str(e) + "\"")
+            except telegram.error.Forbidden as e2:
+                logger.error("Tried to broadcast to chat with id " + str(chat.id) +
+                             " but Telegram-API responded with \"BadRequest: " + str(e2) + "\""
+                             "User has propably blocked bot so broadcast is disabled in the chat.")
+                chat.broadcast_enabled = False
+                chat.save()
+
--- bobweb/bob/command_epic_games.py
+++ bobweb/bob/command_epic_games.py
@@ -1,3 +1,4 @@
+import asyncio
 import io
 import logging
 from datetime import datetime
@@ -10,7 +11,8 @@ from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import CallbackContext
 
-from bobweb.bob import async_http
+from bobweb.bob import async_http, database
+from bobweb.bob.broadcaster import broadcast_to_chats
 from bobweb.bob.command import ChatCommand, regex_simple_command
 from bobweb.bob.command_image_generation import image_to_byte_array
 from bobweb.bob.utils_common import fitzstr_from, has, flatten, dict_search
@@ -29,7 +31,7 @@ class EpicGamesOffersCommand(ChatCommand):
 
     async def handle_update(self, update: Update, context: CallbackContext = None) -> None:
         try:
-            msg, image_bytes = await create_free_games_announcement_msg()
+            msg, image_bytes = await find_current_free_game_offers_and_create_message()
             if has(image_bytes):
                 await update.effective_message.reply_photo(photo=image_bytes, caption=msg, parse_mode=ParseMode.HTML, quote=False)
             else:
@@ -37,11 +39,16 @@ class EpicGamesOffersCommand(ChatCommand):
         except ClientResponseError as e:
             log_msg = f'Epic Games Api error. [status]: {str(e.status)}, [message]: {e.message}, [headers]: {e.headers}'
             logger.exception(log_msg, exc_info=True)
-            await update.effective_message.reply_text(fetch_failed_msg, quote=False)
+            await update.effective_message.reply_text(fetch_failed_no_connection_msg, quote=False)
+        except Exception as e:
+            log_msg = f'Epic Games error: {str(e)}'
+            logger.exception(log_msg, exc_info=True)
+            await update.effective_message.reply_text(fetch_or_processing_failed_msg, quote=False)
 
 
-fetch_failed_msg = 'Ilmaisten eeppisten pelien haku epäonnistui 🔌✂️'
-fetch_ok_no_free_games = 'Ilmaisia eeppisiä pelejä ei ole tällä hetkellä tarjolla 👾'
+fetch_or_processing_failed_msg = 'Ilmaisten eeppisten pelien haku tai tietojen prosessointi epäonnistui 🔌✂️'
+fetch_failed_no_connection_msg = 'Ilmaisten eeppisten pelien palveluun ei onnistuttu muodostamaan yhteyttä  🔌✂️'
+fetch_ok_no_free_games = 'Uusia ilmaisia eeppisiä pelejä ei ole tällä hetkellä tarjolla 👾'
 epic_free_games_api_endpoint = 'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?country=FI'
 epic_games_store_product_base_url = 'https://store.epicgames.com/en-US/p/'
 epic_games_store_free_games_page_url = 'https://store.epicgames.com/en-US/free-games'
@@ -56,25 +63,100 @@ class EpicGamesOffer:
                  page_slug: str,
                  image_tall_url: str,
                  image_thumbnail_url: str):
-        self.title = title
-        self.description = description
-        self.starts_at = starts_at
-        self.ends_at = ends_at
-        self.page_slug = page_slug
-        self.vertical_img_url = image_tall_url
-        self.horizontal_img_url = image_thumbnail_url
+        self.title: str = title
+        self.description: str = description
+        self.starts_at: datetime = starts_at
+        self.ends_at: datetime = ends_at
+        self.page_slug: str = page_slug
+        self.vertical_img_url: str = image_tall_url
+        self.horizontal_img_url: str = image_thumbnail_url
 
 
-async def create_free_games_announcement_msg() -> tuple[str, bytes | None]:
-    games = await fetch_free_epic_games_offering()
+class NoNewFreeGamesError(Exception):
+    """ Simple error class for situation where new games are expected and no new games are found """
+    pass
+
+
+async def daily_announce_new_free_epic_games_store_games(context: CallbackContext):
+    """ Tries to find and announce all new game offers starting on current date for 5 minutes. Request offers from Epic
+        Games Api once a minute. If no new games are found after 5 minutes no announcement is made. If all requests fail
+        and no successful response is gotten, announces failure after 5 minutes is up. Returns immediately after
+        successful delivery. This is done to ensure that the bot announces new game offers as soon as possible. """
+    chats_with_announcement_on = [x for x in database.get_chats() if x.free_game_offers_enabled]
+    if len(chats_with_announcement_on) == 0:
+        return  # Early return if no chats with setting turned on
+
+    max_try_count = 5
+    try_count = 0
+    delay_seconds = 60
+
+    # Possible status'
+    client_response_error = None
+    response_ok_no_new_games = False
+
+    while try_count < max_try_count:
+        # Define either announcement message and possible game images or fetch_failed_msg without image
+        try_count += 1
+        try:
+            msg, image_bytes = await find_new_free_game_offers_and_create_message()
+            await broadcast_to_chats(context.bot, chats_with_announcement_on, msg, image_bytes)
+            return  # Early return after successful announcement
+        except ClientResponseError as e:
+            # Set client_response_error. If no successful request is done with time period,
+            # connection error message is sent
+            client_response_error = e
+        except NoNewFreeGamesError:
+            # If no new games are found. As this means successful request with response,
+            # client_response_error is overriden
+            client_response_error = None
+            response_ok_no_new_games = True
+        except Exception as e:
+            log_msg = f'Epic Games error: {str(e)}'
+            logger.exception(log_msg, exc_info=True)
+            await broadcast_to_chats(context.bot, chats_with_announcement_on, fetch_or_processing_failed_msg)
+            return  # Most likely not going to be fixed with trying again. So no retries for other exceptions
+        finally:
+            if try_count < max_try_count:
+                await asyncio.sleep(delay_seconds)
+
+    if client_response_error is not None:
+        log_msg = (f'Epic Games Api error. [status]: {str(client_response_error.status)}, [message]: '
+                   f'{client_response_error.message}, [headers]: {client_response_error.headers}')
+        logger.exception(log_msg, exc_info=True)
+        await broadcast_to_chats(context.bot, chats_with_announcement_on, fetch_failed_no_connection_msg)
+
+    elif response_ok_no_new_games:
+        logger.info('Epic games offers status fetched successfully but no new free games found')
+        is_thursday = datetime.today().weekday() == 3  # Monday == 0 ... Sunday == 6
+        if is_thursday:
+            # Only if it's thursday, should there be announcement that no games were found.
+            # On other week days it is the expected outcome
+            await broadcast_to_chats(context.bot, chats_with_announcement_on, fetch_ok_no_free_games)
+
+
+async def find_new_free_game_offers_and_create_message() -> tuple[str, bytes | None]:
+    """ Finds all new free game offers starting today. If none is found, or fetch fails, raises an exception """
+    games: list[EpicGamesOffer] = await fetch_free_epic_games_offering(only_offers_starting_today=True)
+    if len(games) == 0:
+        raise NoNewFreeGamesError()
+
+    return await create_message_and_game_image_compilation(games, '📬 Uudet ilmaiset eeppiset pelit 📩')
+
+
+async def find_current_free_game_offers_and_create_message() -> tuple[str, bytes | None]:
+    """ Creates message with current free game offers """
+    games: list[EpicGamesOffer] = await fetch_free_epic_games_offering()
     if len(games) == 0:
         return fetch_ok_no_free_games, None
-    else:
-        heading = '📬 Viikon ilmaiset eeppiset pelit 📩'
-        msg = heading + format_games_offer_list(games)
-        msg_image = await get_game_offers_image(games)
-        image_bytes = image_to_byte_array(msg_image)
-        return msg, image_bytes
+
+    return await create_message_and_game_image_compilation(games, '📬 Ilmaiset eeppiset pelit 📩')
+
+
+async def create_message_and_game_image_compilation(games: List[EpicGamesOffer], heading: str) -> Tuple[str, bytes]:
+    msg = heading + format_games_offer_list(games)
+    msg_image = await get_game_offers_image(games)
+    image_bytes = image_to_byte_array(msg_image)
+    return msg, image_bytes
 
 
 def format_games_offer_list(games: list[EpicGamesOffer]):
@@ -88,7 +170,8 @@ def format_games_offer_list(games: list[EpicGamesOffer]):
     return game_list
 
 
-async def fetch_free_epic_games_offering() -> list[EpicGamesOffer]:
+async def fetch_free_epic_games_offering(only_offers_starting_today: bool = False) -> list[EpicGamesOffer]:
+    today: datetime.date = datetime.today().date()
     content: dict = await async_http.fetch_json(epic_free_games_api_endpoint)
     # use None-safe dict-get-chain that returns list if any key is not found
     game_dict_list = dict_search(content, 'data', 'Catalog', 'searchStore', 'elements') or []
@@ -96,7 +179,10 @@ async def fetch_free_epic_games_offering() -> list[EpicGamesOffer]:
     game_offers = []
     for d in game_dict_list:
         game_offer: EpicGamesOffer = extract_free_game_offer_from_game_dict(d)
-        if game_offer is not None:
+        # is added to the list if:
+        # - has promotion
+        # - only_offers_starting_today is false or if it is set to be true and promotion starts on current day
+        if game_offer is not None and (only_offers_starting_today is False or game_offer.starts_at.date() == today):
             game_offers.append(game_offer)
 
     return game_offers
