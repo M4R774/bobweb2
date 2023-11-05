37a0e77d07e76fccd0a91159abccbe61f3c462b6
Lauri Viitanen <lauri_viitanen@hotmail.com>
2023-11-05 12:14:35 +0200

    - Added env variable export to docker container for Telegram Client Id and Hash

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Taking back ups from the db"
+echo "[" + date + "]: Taking back ups from the db"
 mkdir -p ../backups
 touch bobweb/web/db.sqlite3
 cp bobweb/web/db.sqlite3 "../backups/$(date +%F_%R).sqlite3"
 
-echo "Starting deployment"
+echo "[" + date + "]: Starting deployment" >> docker-compose.log
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 COMMIT_AUTHOR_NAME=$(git log -1 --pretty=%an)
 COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty=%ae)
@@ -15,7 +15,7 @@ export COMMIT_AUTHOR_EMAIL
 
 CPU_architecture=$(uname -m)
 if [[ $CPU_architecture == 'armv7l' ]]; then
-  docker-compose -f docker-compose.yml up --build --detach --force-recreate --remove-orphans |& tee docker-compose.log
+  docker-compose -f docker-compose.yml up --build --detach --force-recreate --remove-orphans &>> docker-compose.log
 else
-  docker-compose -f ci.docker-compose.yml up --build --detach --force-recreate --remove-orphans |& tee docker-compose.log
+  docker-compose -f ci.docker-compose.yml up --build --detach --force-recreate --remove-orphans &>> docker-compose.log
 fi
--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Starting deployment"
-docker-compose -f dev.docker-compose.yml up --build --detach --force-recreate --remove-orphans |& tee docker-compose.log
+echo "[" + date + "]: Starting deployment" >> docker-compose.log
+docker-compose -f dev.docker-compose.yml up --build --detach --force-recreate --remove-orphans &>> docker-compose.log
