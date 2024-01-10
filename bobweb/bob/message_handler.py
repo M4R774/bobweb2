46e30256dd630b094a333a12d2d0bb8ffa1f8182
Lauri Viitanen <lauri_viitanen@hotmail.com>
2024-01-10 19:24:11 +0200

    - Added None-check to git-promotions handling checks

--- bobweb/bob/message_handler.py
+++ bobweb/bob/message_handler.py
@@ -75,9 +75,11 @@ async def reply_handler(update: Update, context: CallbackContext = None):
     # Test if reply target is current days daily question. If so, save update as answer
     await check_and_handle_reply_to_daily_question(update, context)
 
-    is_reply_to_bob = has(context) and update.effective_message.reply_to_message.from_user.id == context.bot.id
+    reply_to_message = update.effective_message.reply_to_message
+    is_reply_to_bob = has(context) and reply_to_message.from_user.id == context.bot.id
     if is_reply_to_bob:
-        if update.effective_message.reply_to_message.text.startswith("Git käyttäjä "):
+        message_text = reply_to_message.text
+        if message_text and message_text.startswith("Git käyttäjä "):
             await git_promotions.process_entities(update)
 
 
