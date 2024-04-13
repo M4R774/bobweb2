6dc287b0b69a355911a311a63746d749888c95f5
Lauri Viitanen <lauri.viitanen@tuni.fi>
2024-04-13 20:01:25 +0300

    202 added more metadata parsing for recipes

--- bobweb/bob/command_ruoka.py
+++ bobweb/bob/command_ruoka.py
@@ -1,13 +1,44 @@
+import logging
 import random
 
+from aiohttp import ClientResponseError
 from bs4 import BeautifulSoup
 from telegram.ext import CallbackContext
 
-from bobweb.bob import async_http
+from bobweb.bob import async_http, utils_common
 from bobweb.bob.message_board import ScheduledMessage
 from bobweb.bob.resources.recipes import recipes
 from bobweb.bob.command import ChatCommand, regex_simple_command_with_parameters
 from telegram import Update
+
+
+logger = logging.getLogger(__name__)
+
+
+class RecipeDetails:
+    """ Represents single recipe details fetched from external service. Some attributes are kept as string as
+        there is no guarantee that recipe metadata exists and/or is in consistent parseable format """
+    def __init__(self,
+                 url: str,
+                 metadata_fetched: bool,
+                 name: str = None,
+                 description: str = None,
+                 servings: int = None,
+                 prep_time: str = None,
+                 difficulty: str = None):
+        self.url: str = url
+        self.metadata_fetched = metadata_fetched
+        self.name: str = name
+        self.description: str = description
+        self.servings: int = servings
+        self.prep_time: str = prep_time
+        self.difficulty: str = difficulty
+
+
+# Soppa 365 labels
+servings_count_label = 'Annoksia'
+preparation_time_label = 'Valmistusaika'
+difficulty_label = 'Vaikeustaso'
 
 
 class RuokaCommand(ChatCommand):
@@ -46,11 +77,40 @@ class RuokaCommand(ChatCommand):
 async def create_message_board_daily_message(chat_id: int = None) -> ScheduledMessage:
     recipe_link = random.choice(recipes)  # NOSONAR
     # Find name link by extracting last part of link after '/' and replacing dashes with spaces
-    html_content = await async_http.fetch_content_text(recipe_link)
+    recipe_details: RecipeDetails = await fetch_and_parse_recipe_details_from_soppa365(recipe_link)
+
+
+async def fetch_and_parse_recipe_details_from_soppa365(recipe_url: str) -> RecipeDetails:
+    """
+    Fetches and parses www.soppa365.fi recipe web page for given url. Html-response is parsed into virtual DOM,
+    from which details of the recipe is searched with selectors. If web page get request fails, returns RecipeDetails
+    with containing only the url and having 'metadata_fetched' == False.
+    """
+    try:
+        html_content = await async_http.fetch_content_text(recipe_url)
+        recipe_details: RecipeDetails = parse_recipe_details(recipe_url, html_content)
+    except ClientResponseError as e:
+        logger.error(f'Tried to fetch recipe web page for url: {recipe_url}. Error:\n{repr(e)}')
+        return RecipeDetails(url=recipe_url, metadata_fetched=False)
+
+    print(f"Servings: {recipe_details.servings}, "
+          f"Prep time: {recipe_details.prep_time}, "
+          f"Difficulty: {recipe_details.difficulty}, "
+          f"Description: {recipe_details.description}")
+
+
+def parse_recipe_details(recipe_url: str, html_content: str) -> RecipeDetails:
+    # Parse html content to virtual DOM and search for the metadata
     html_dom = BeautifulSoup(html_content, 'html.parser')
 
-    # Find first 'group-recipe-info' on the page
-    recipe_info_div = html_dom.find_all('div', class_='group-recipe-info')[0]
+    # Find recipe name
+    h1_element = html_dom.find('h1')
+    recipe_name = h1_element.find('a').text
+
+    # Find first 'group-recipe-info' on the page. Single page response from Soppa 365 contains multiple recipes
+    # where the requested recipe is first in the page.
+    recipe_info_elements = html_dom.find_all('div', class_='group-recipe-info')
+    recipe_info_div = recipe_info_elements[0]
     # Extract the next siblings 'div.field-items' text of each 'div.field-label'
     labels = recipe_info_div.find_all('div', class_='field-label')
     data = {}
@@ -61,18 +121,24 @@ async def create_message_board_daily_message(chat_id: int = None) -> ScheduledMe
             data[label.get_text(strip=True)] = value
 
     # Print or use the values extracted, use the exact labels to access them
-    servings = data['Annoksia']
-    prep_time = data['Valmistusaika']
-    difficulty = data['Vaikeustaso']
+    servings = data[servings_count_label]
+    prep_time = data[preparation_time_label]
+    difficulty = data[difficulty_label]
 
     # Find description
-    description_meta_tags = html_dom.find_all('meta', attrs={'name': 'description'})[0]
-    first_description_content = description_meta_tags['content']
+    description_meta_tags = html_dom.find_all('meta', attrs={'name': 'description'})
+    first_description_content = utils_common.object_search(description_meta_tags, 0, 'attrs', 'content')
 
-    print(f"Servings: {servings}, Prep time: {prep_time}, Difficulty: {difficulty}, Description: {first_description_content}")
+    return RecipeDetails(
+        url=recipe_url,
+        metadata_fetched=True,
+        name=recipe_name,
+        description=first_description_content,
+        servings=servings,
+        prep_time=prep_time,
+        difficulty=difficulty
 
-    # recipe_name = recipe_link.split('/')[-1].replace('-', ' ')
-    # Try to find additional details of the recipe
+    )
 
 # EXAMPLE
 #
