5318e60f6a2ea2f967bd56ec8b195fcc26f8d6a8
Latemus <lauri_viitanen@hotmail.com>
2024-05-20 22:15:37 +0300

    Fixes to deployment scripts: (#272)
    - Changed legacy `docker-compose` command to newer `docker compose` that is bundled with Docker. This fixed errors with `deploy.prod.from_dockerhub.sh` script
    - Tweaks and updated to readme.md in project root. Ran through a formatter.

--- deploy.prod.from_dockerhub.sh
+++ deploy.prod.from_dockerhub.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# Note! As this file might be referenced from a cron job running on bots production environment, any path or name
+# changes are breaking and require manual change to the cron job on the running environment.
+# If deployments are done with images built in github actions and stored in dockerhub, this is the only file from the
+# project repository needed on the server host machine.
+
 # script for checking latest version of docker image from dockerhub and then
 # deploying it if there is newer image available. Can be ran for example as a cron job.
 # To edit cron jobs on linux based system run command `crontab -e` and then add new cron job row.
 #
 # Example of cron-job configuration for running script at every 5 minutes:
-# */5 * * * * /{path_to_this_script}/deploy_from_dockerhub.sh >> /{path_to_any_location_for_logs}/deploy_from_dockerhub.log
-
-# Note! As this file might be referenced from a cron job running on bots production environment, any path or name
-# changes are breaking and require manual change to the cron job.
+# */5 * * * *  cd /{path-to-the-root-of-the-project} && ./deploy.prod.from_dockerhub.sh  >> deploy.prod.from_dockerhub.log
+# Explanation: every 5 minutes run following command. 1. CD to defined path, 2. run this script while
+# logging this script run to a log file
 
 # Define the Docker image name and tag. Only main-tagged images are checked
 IMAGE_NAME="latemus/bobweb2:main"
+LOG_FILE_PATH="docker-compose.prod.dockerhub.log"
 
 # Function to check if a new version is available
 function is_new_version_available() {
@@ -23,29 +28,33 @@ function is_new_version_available() {
     fi
 }
 
-# Check if a new version is available
-if is_new_version_available; then
-  # If so, do same deployment as in deploy.prod.sh
-  # however this uses different different docker-compose configuration
-  {
-    echo -e "[$(date)]: New version of $IMAGE_NAME is available. Deploying."
-    echo "Taking back ups from the db"
-    mkdir -p ../backups
-    touch bobweb/web/db.sqlite3
-    cp bobweb/web/db.sqlite3 "../backups/$(date +%F_%R).sqlite3"
 
-    COMMIT_MESSAGE=$(git log -1 --pretty=%B)
-    COMMIT_AUTHOR_NAME=$(git log -1 --pretty=%an)
-    COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty=%ae)
-    export COMMIT_MESSAGE
-    export COMMIT_AUTHOR_NAME
-    export COMMIT_AUTHOR_EMAIL
+{
+  # Check if a new version is available
+  if is_new_version_available;
+    then
+        # If so, do same deployment as in deploy.prod.sh
+        # however this uses different different docker compose configuration
+        echo -e "[$(date)]: New version of $IMAGE_NAME is available. Deploying."
+        echo "Taking back ups from the db"
+        # create new backups and 'bobweb/web' folders if theynot exists
+        mkdir -p bobweb/web
+        touch bobweb/web/db.sqlite3
+        mkdir -p ../backups
+        cp bobweb/web/db.sqlite3 "../backups/$(date +%F_%R).sqlite3"
 
-    # Note! Without `--build`-flag as the image is fetched from dockerhub
-    docker-compose -f docker-compose.prod.dockerhub.yml up --detach --force-recreate --remove-orphans
-    echo -e "[$(date)]: Deployment done"
-  } |& tee docker-compose.dockerhub.log
+        COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+        COMMIT_AUTHOR_NAME=$(git log -1 --pretty=%an)
+        COMMIT_AUTHOR_EMAIL=$(git log -1 --pretty=%ae)
+        export COMMIT_MESSAGE
+        export COMMIT_AUTHOR_NAME
+        export COMMIT_AUTHOR_EMAIL
 
-else
-    echo -e "[$(date)]: No new version available. No action taken." |& tee docker-compose.dockerhub.log
-fi
+        # Note! Without `--build`-flag as the image is fetched from dockerhub
+        # Note! uses 'docker compose', not 'docker-compose' -command
+        docker compose -f docker-compose.prod.dockerhub.yml up --detach --force-recreate --remove-orphans
+        echo -e "[$(date)]: Deployment done"
+    else
+        echo -e "[$(date)]: No new version available. No action taken."
+  fi
+} |& tee $LOG_FILE_PATH
