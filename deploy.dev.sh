e9ad9b28d2e2cd668be127129e814f74cd58c6b0
Lauri Viitanen <lauri_viitanen@hotmail.com>
2024-12-16 22:23:48 +0200

    * Add creating new database to deploy.dev.sh

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo -e "\n\n\n[$(date)]: Starting deployment"
+touch bobweb/web/db.sqlite3
 docker compose -f docker-compose.dev.yml up --build --force-recreate --remove-orphans
 echo -e "[$(date)]: Deployment finished"
