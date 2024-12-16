7f9de81aa51ca93501884e9f32f064a9528c48f5
Lauri Viitanen <lauri_viitanen@hotmail.com>
2024-12-16 21:40:15 +0200

    * Changed Dockerfile package versions * Removed logging from deploy.dev.sh and detached mode as it might cause problems with `docker_lint_and_smoke_test` job

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-{
-  echo -e "\n\n\n[$(date)]: Starting deployment"
-  docker compose -f docker-compose.dev.yml up --build --detach --force-recreate --remove-orphans
-  echo -e "[$(date)]: Deployment finished"
-} > docker-compose.dev.log 2>&1
+echo -e "\n\n\n[$(date)]: Starting deployment"
+docker compose -f docker-compose.dev.yml up --build --force-recreate --remove-orphans
+echo -e "[$(date)]: Deployment finished"
