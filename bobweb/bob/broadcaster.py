d692971e42fe0de16e398c993f669a5fed41f661
Lauri Viitanen <lauri.viitanen@tuni.fi>
2024-12-31 11:26:01 +0200

    * Add expandable quote to commit message broadcast

--- bobweb/bob/broadcaster.py
+++ bobweb/bob/broadcaster.py
@@ -11,10 +11,10 @@ from bobweb.web.bobapp.models import Chat, TelegramUser
 logger = logging.getLogger(__name__)
 
 
-async def broadcast(bot: Bot, text: str):
+async def broadcast(bot: Bot, text: str, parse_mode: ParseMode = None):
     """ Broadcasts fo all chats known to bot that have broadcast enabled """
     chats: List[int] = [chat.id for chat in database.get_chats() if chat.broadcast_enabled]
-    await broadcast_to_chats(bot, chats, text)
+    await broadcast_to_chats(bot, chats, text, parse_mode=parse_mode)
 
 
 async def broadcast_to_chats(bot: Bot,
