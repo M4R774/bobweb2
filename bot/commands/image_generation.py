6f830215c53c0a1f044f7bcdaacc1eb44ef5f98a
Lauri Viitanen <lauri_viitanen@hotmail.com>
2025-09-29 23:11:11 +0300

    * Fix issue brought up by sonar issue fixes

--- bot/commands/image_generation.py
+++ bot/commands/image_generation.py
@@ -149,6 +149,6 @@ def image_to_byte_array(image: Image) -> Optional[bytes]:
 
 def remove_all_dalle_commands_related_text(text: str) -> str:
     text = re.sub(f'({DalleCommand.regex})', '', text)
-    text = str.replace('"<i>', '', text)
-    text = str.replace('</i>"', '', text)
+    text = text.replace('"<i>', '')
+    text = text.replace('</i>"', '')
     return text.strip()
